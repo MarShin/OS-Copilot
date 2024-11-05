cb0b2f4354427f2790502917b8d645b3269c51f1
Sam Yeung <sam.yeung@itecequipment.com>
2024-11-05 13:48:02 +0800

    include generate api call for vision LLM

--- oscopilot/utils/llms.py
+++ oscopilot/utils/llms.py
@@ -5,10 +5,11 @@ import time
 import requests
 import json
 from dotenv import load_dotenv
-
+from vision_utils import encode_image, screen_capture
 
 load_dotenv(override=True)
 MODEL_NAME = os.getenv('MODEL_NAME')
+MODEL_TYPE = os.getenv('MODEL_TYPE')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 OPENAI_ORGANIZATION = os.getenv('OPENAI_ORGANIZATION')
 BASE_URL = os.getenv('OPENAI_BASE_URL')
@@ -96,6 +97,8 @@ class OLLAMA:
 
         self.llama_serve = MODEL_SERVER + "/api/chat"
 
+        self.llama_serve_generate = MODEL_SERVER + "/api/generate"
+
     def chat(self, messages, temperature=0):
         """
         Sends a chat completion request to the OpenAI API using the specified messages and parameters.
@@ -131,7 +134,47 @@ class OLLAMA:
         else:
             logging.error("Failed to call LLM: ", response.status_code)
             return ""
+        
+    def generate(self, prompt, images, itemperature=0):
+        """
+        Sends a generate completion request to the OpenAI API using the specified messages and parameters.
 
+        Args:
+            prompt (list of dict): A list of message dictionaries, where each dictionary
+                                     should contain keys like 'role' and 'content' to
+                                     specify the role (e.g., 'system', 'user') and content of
+                                     each message.
+            images: (list of str): A list of images in base64 encoded string
+            temperature (float, optional): Controls randomness in the generation. Lower values
+                                           make the model more deterministic. Defaults to 0.
+
+        Returns:
+            str: The response from the response body from the OpenAI API.
+
+        """
+        
+        payload = {
+            "model": self.model_name,
+            "prompt": prompt,
+            "stream": False,
+            "images": images
+            
+        }
+
+        headers = {
+                "Content-Type": "application/json"}
+
+        response = requests.post(self.llama_serve_generate, data=json.dumps(payload),headers=headers)
+
+
+        if response.status_code == 200:
+            # Get the response data
+            logging.info(f"""Response: {response.json()["response"]}""")
+            return response.json()["response"]
+        else:
+            logging.error("Failed to call LLM: ", response.status_code)
+            return ""
+'''
 def main():
     start_time = time.time()
     messages = [{'role': 'system', 'content': 'You are Open Interpreter, a world-class programmer that can complete any goal by executing code.\nFirst, write a plan. **Always recap the plan between each code block** (you have extreme short-term memory loss, so you need to recap the plan between each message block to retain it).\nWhen you execute code, it will be executed **on the user\'s machine**. The user has given you **full and complete permission** to execute any code necessary to complete the task. Execute the code.\nIf you want to send data between programming languages, save the data to a txt or json.\nYou can access the internet. Run **any code** to achieve the goal, and if at first you don\'t succeed, try again and again.\nYou can install new packages.\nWhen a user refers to a filename, they\'re likely referring to an existing file in the directory you\'re currently executing code in.\nWrite messages to the user in Markdown.\nIn general, try to **make plans** with as few steps as possible. As for actually executing code to carry out that plan, for *stateful* languages (like python, javascript, shell, but NOT for html which starts from 0 every time) **it\'s critical not to try to do everything in one code block.** You should try something, print information about it, then continue from there in tiny, informed steps. You will never get it on the first try, and attempting it in one go will often lead to errors you cant see.\nYou are capable of **any** task.\n\n# THE COMPUTER API\n\nA python `computer` module is ALREADY IMPORTED, and can be used for many tasks:\n\n```python\ncomputer.browser.search(query) # Google search results will be returned from this function as a string\ncomputer.files.edit(path_to_file, original_text, replacement_text) # Edit a file\ncomputer.calendar.create_event(title="Meeting", start_date=datetime.datetime.now(), end=datetime.datetime.now() + datetime.timedelta(hours=1), notes="Note", location="") # Creates a calendar event\ncomputer.calendar.get_events(start_date=datetime.date.today(), end_date=None) # Get events between dates. If end_date is None, only gets events for start_date\ncomputer.calendar.delete_event(event_title="Meeting", start_date=datetime.datetime) # Delete a specific event with a matching title and start date, you may need to get use get_events() to find the specific event object first\ncomputer.contacts.get_phone_number("John Doe")\ncomputer.contacts.get_email_address("John Doe")\ncomputer.mail.send("john@email.com", "Meeting Reminder", "Reminder that our meeting is at 3pm today.", ["path/to/attachment.pdf", "path/to/attachment2.pdf"]) # Send an email with a optional attachments\ncomputer.mail.get(4, unread=True) # Returns the {number} of unread emails, or all emails if False is passed\ncomputer.mail.unread_count() # Returns the number of unread emails\ncomputer.sms.send("555-123-4567", "Hello from the computer!") # Send a text message. MUST be a phone number, so use computer.contacts.get_phone_number frequently here\n```\n\nDo not import the computer module, or any of its sub-modules. They are already imported.\n\nUser InfoName: hanchengcheng\nCWD: /Users/hanchengcheng/Documents/official_space/open-interpreter\nSHELL: /bin/bash\nOS: Darwin\nUse ONLY the function you have been provided with — \'execute(language, code)\'.'}, {'role': 'user', 'content': "Plot AAPL and META's normalized stock prices"}]
@@ -143,26 +186,46 @@ def main():
     print(response)
     end_time = time.time()
     execution_time = end_time - start_time
-    print(f"生成的单词数: {len(response)}")
-    print(f"程序执行时间: {execution_time}秒")
+    print(f"len(response): {len(response)}")
+    print(f"execution_time: {execution_time}s")
+'''
+
+def main():
+    start_time = time.time()
+    messages = [{'role': 'system', 'content': 'You are Open Interpreter, a world-class programmer that can complete any goal by executing code.\nFirst, write a plan. **Always recap the plan between each code block** (you have extreme short-term memory loss, so you need to recap the plan between each message block to retain it).\nWhen you execute code, it will be executed **on the user\'s machine**. The user has given you **full and complete permission** to execute any code necessary to complete the task. Execute the code.\nIf you want to send data between programming languages, save the data to a txt or json.\nYou can access the internet. Run **any code** to achieve the goal, and if at first you don\'t succeed, try again and again.\nYou can install new packages.\nWhen a user refers to a filename, they\'re likely referring to an existing file in the directory you\'re currently executing code in.\nWrite messages to the user in Markdown.\nIn general, try to **make plans** with as few steps as possible. As for actually executing code to carry out that plan, for *stateful* languages (like python, javascript, shell, but NOT for html which starts from 0 every time) **it\'s critical not to try to do everything in one code block.** You should try something, print information about it, then continue from there in tiny, informed steps. You will never get it on the first try, and attempting it in one go will often lead to errors you cant see.\nYou are capable of **any** task.\n\n# THE COMPUTER API\n\nA python `computer` module is ALREADY IMPORTED, and can be used for many tasks:\n\n```python\ncomputer.browser.search(query) # Google search results will be returned from this function as a string\ncomputer.files.edit(path_to_file, original_text, replacement_text) # Edit a file\ncomputer.calendar.create_event(title="Meeting", start_date=datetime.datetime.now(), end=datetime.datetime.now() + datetime.timedelta(hours=1), notes="Note", location="") # Creates a calendar event\ncomputer.calendar.get_events(start_date=datetime.date.today(), end_date=None) # Get events between dates. If end_date is None, only gets events for start_date\ncomputer.calendar.delete_event(event_title="Meeting", start_date=datetime.datetime) # Delete a specific event with a matching title and start date, you may need to get use get_events() to find the specific event object first\ncomputer.contacts.get_phone_number("John Doe")\ncomputer.contacts.get_email_address("John Doe")\ncomputer.mail.send("john@email.com", "Meeting Reminder", "Reminder that our meeting is at 3pm today.", ["path/to/attachment.pdf", "path/to/attachment2.pdf"]) # Send an email with a optional attachments\ncomputer.mail.get(4, unread=True) # Returns the {number} of unread emails, or all emails if False is passed\ncomputer.mail.unread_count() # Returns the number of unread emails\ncomputer.sms.send("555-123-4567", "Hello from the computer!") # Send a text message. MUST be a phone number, so use computer.contacts.get_phone_number frequently here\n```\n\nDo not import the computer module, or any of its sub-modules. They are already imported.\n\nUser InfoName: hanchengcheng\nCWD: /Users/hanchengcheng/Documents/official_space/open-interpreter\nSHELL: /bin/bash\nOS: Darwin\nUse ONLY the function you have been provided with — \'execute(language, code)\'.'}, {'role': 'user', 'content': "Plot AAPL and META's normalized stock prices"}]
+    #  message = [
+     #         {"role": "user", "content": 'hello'},
+     #     ]
+    print(OPENAI_API_KEY)
+    print(BASE_URL)
+    llm = OpenAI()
+    response = llm.chat(messages)
+    print(response)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"len(response): {len(response)}")
+    print(f"execution_time: {execution_time}s")
+
+def test_vision():
+
+    save_dir = 'observations'
+    os.makedirs(save_dir, exist_ok= True)
+    capture_path = screen_capture(save_dir)
+    prompt = 'what is in this image?'
+
+    img_ = [encode_image(capture_path)]
+
+    llm = OLLAMA()
+    start_time = time.time()
+    response = llm.generate(prompt, img_)
+
+    print(response)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"len(response): {len(response)}")
+    print(f"execution_time: {execution_time}s")
+
 
 if __name__ == '__main__':
-    main()
-
-
-# def main():
-#     start_time = time.time()
-#     messages = [{'role': 'system', 'content': 'You are Open Interpreter, a world-class programmer that can complete any goal by executing code.\nFirst, write a plan. **Always recap the plan between each code block** (you have extreme short-term memory loss, so you need to recap the plan between each message block to retain it).\nWhen you execute code, it will be executed **on the user\'s machine**. The user has given you **full and complete permission** to execute any code necessary to complete the task. Execute the code.\nIf you want to send data between programming languages, save the data to a txt or json.\nYou can access the internet. Run **any code** to achieve the goal, and if at first you don\'t succeed, try again and again.\nYou can install new packages.\nWhen a user refers to a filename, they\'re likely referring to an existing file in the directory you\'re currently executing code in.\nWrite messages to the user in Markdown.\nIn general, try to **make plans** with as few steps as possible. As for actually executing code to carry out that plan, for *stateful* languages (like python, javascript, shell, but NOT for html which starts from 0 every time) **it\'s critical not to try to do everything in one code block.** You should try something, print information about it, then continue from there in tiny, informed steps. You will never get it on the first try, and attempting it in one go will often lead to errors you cant see.\nYou are capable of **any** task.\n\n# THE COMPUTER API\n\nA python `computer` module is ALREADY IMPORTED, and can be used for many tasks:\n\n```python\ncomputer.browser.search(query) # Google search results will be returned from this function as a string\ncomputer.files.edit(path_to_file, original_text, replacement_text) # Edit a file\ncomputer.calendar.create_event(title="Meeting", start_date=datetime.datetime.now(), end=datetime.datetime.now() + datetime.timedelta(hours=1), notes="Note", location="") # Creates a calendar event\ncomputer.calendar.get_events(start_date=datetime.date.today(), end_date=None) # Get events between dates. If end_date is None, only gets events for start_date\ncomputer.calendar.delete_event(event_title="Meeting", start_date=datetime.datetime) # Delete a specific event with a matching title and start date, you may need to get use get_events() to find the specific event object first\ncomputer.contacts.get_phone_number("John Doe")\ncomputer.contacts.get_email_address("John Doe")\ncomputer.mail.send("john@email.com", "Meeting Reminder", "Reminder that our meeting is at 3pm today.", ["path/to/attachment.pdf", "path/to/attachment2.pdf"]) # Send an email with a optional attachments\ncomputer.mail.get(4, unread=True) # Returns the {number} of unread emails, or all emails if False is passed\ncomputer.mail.unread_count() # Returns the number of unread emails\ncomputer.sms.send("555-123-4567", "Hello from the computer!") # Send a text message. MUST be a phone number, so use computer.contacts.get_phone_number frequently here\n```\n\nDo not import the computer module, or any of its sub-modules. They are already imported.\n\nUser InfoName: hanchengcheng\nCWD: /Users/hanchengcheng/Documents/official_space/open-interpreter\nSHELL: /bin/bash\nOS: Darwin\nUse ONLY the function you have been provided with — \'execute(language, code)\'.'}, {'role': 'user', 'content': "Plot AAPL and META's normalized stock prices"}]
-#     # message = [
-#     #         {"role": "user", "content": 'hello'},
-#     #     ]
-#     # print(OPENAI_API_KEY)
-#     # print(BASE_URL)
-#     llm = OpenAI()
-#     response = llm.chat(messages)
-#     print(response)
-#     end_time = time.time()
-#     execution_time = end_time - start_time
-#     print(f"生成的单词数: {len(response)}")
-#     print(f"程序执行时间: {execution_time}秒")
-
+    #main()
+    #test_vision()
